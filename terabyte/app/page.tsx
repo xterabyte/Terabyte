464c120e5f070b895e6a0af9a45704a2fae3da89
xterabyte <admin@terabyte.africa>
2024-11-03 11:31:44 +0000

    orifinal initial commit

---
+++ terabyte/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1>This is the Begining</h1>
+      
+      <p> Hello world </p>
+      <p> This is me still orking to ensure that the page doesn't just shut down</p>
+    </div>
+  );
+}
