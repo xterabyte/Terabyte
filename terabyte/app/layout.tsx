e182d15f8c7631cd3874b80a6258ce8a97ef06a0
xterabyte <admin@terabyte.africa>
2024-11-04 00:22:22 +0000

    change default theme

--- terabyte/app/layout.tsx
+++ terabyte/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <body className={`${montserrat.variable} font-sans`}>
         <ThemeProvider
               attribute="class"
-              defaultTheme="system"
+              defaultTheme="light"
               enableSystem
               disableTransitionOnChange
             >
