19aaffb745145c3f46222334530912f1fd6467ba
xterabyte <admin@terabyte.africa>
2024-11-04 00:07:03 +0000

    Add the hero page

--- terabyte/app/page.tsx
+++ terabyte/app/page.tsx
@@ -1,12 +1,10 @@
-
+import Hero from "./components/Hero"
 
 export default function Home() {
   return (
-    <div className="cyan-900">
-      <h1>This is the Begining</h1>
-      
-      <p> Hello world </p>
-      <p> This is me still orking to ensure that the page doesn't just shut down</p>
-    </div>
-  );
+    <main>
+      <Hero />
+      {/* Other sections will go here */}
+    </main>
+  )
 }
